5546ed70add6f6b33fb6afe59351d8a61a2f4d1a
Welton Rodrigues <67546207+SrNotlew@users.noreply.github.com>
2022-10-28 21:16:38 -0300

    Ajustando Admin. Close 5#

--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,17 @@
+import imp
 from django.contrib import admin
-
+from .models import Produto
 # Register your models here.
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = (
+        'importado',
+        'ncm',
+        'produto',
+        'preco',
+        'estoque',
+        'estoque_minimo',
+    )
+    search_fields = ('produto',)
+    list_filter = ('importado',)
